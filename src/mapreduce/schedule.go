a4f6c4b84d859e78911f259dce139c78e719a87c
gaojm <gjm_kevin@163.com>
2017-04-21 16:05:09 +0800

    lab1 partIII without test

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -24,5 +27,57 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	/*
+	   调用两次一次map,一次reduce
+	   可能任务比worker的数量多，所以需要给worker一个任务队列，每次一个，所有任务都成功才能返回
+	   通过读取registerChan 获取worker信息，里面包含worker信息，如rpc addr.
+	   有一些worker也许在schedule执行前就已经在registerChan中，有些则在执行过程中新添加
+	   schedule需要利用所有的worker,包括启动后添加的
+	*/
+	/*
+		Master参数分析：
+		workers:注册到master中所有的worker
+		registerChannel:注册到服务器的所有worker,与worker不同在于worker注册完成后就不会变化，registerChannel在线程中异步添加，可以重复添加
+		所以，取可用worker直接从registerChannel获取即可，不需要读取workers
+	*/
+	/*
+		实现分析：
+			1.使用RPC调用worker的函数，可以仿注册函数
+			2.异步执行所有的worker，所以每一个RPC的调用需要启线程
+			3.任务可能比worker多，所以worker执行完成后需要让其他任务继续使用
+			3.中间执行过程中若有失败情况，则需要重新执行此项任务
+			4.实验手册中提示：Hint: You may find sync.WaitGroup useful.
+	*/
+
+	//ok := call(availableworker, "Worker.DoTask", args, new(struct{}))
+	var wg sync.WaitGroup
+
+	for i := 0; i < ntasks; i++ {
+		go func(i int) {
+			wg.Add(1)
+			args := new(DoTaskArgs)
+			if phase == "mapPhase" {
+				args.File = mr.files[i]
+			}
+			args.JobName = mr.jobName
+			args.NumOtherPhase = nios
+			args.Phase = phase
+			args.TaskNumber = i
+			for {
+				idleworker := <-mr.registerChannel
+				ok := call(idleworker, "Worker.DoTask", args, new(struct{}))
+				if ok {
+					wg.Done()
+				}
+				///XXX
+				///放在这里还是if里面？？错误单纯的是计算错误？还是worker本身的错误？？
+				mr.registerChannel <- idleworker
+			}
+
+		}(i)
+	}
+
+	wg.Wait()
+
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
