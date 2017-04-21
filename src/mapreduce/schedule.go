fa91cf76708ef9e6d9c6c6451406617564a02377
gaojm <gjm_kevin@163.com>
2017-04-21 17:31:23 +0800

    debug

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -2,7 +2,6 @@ package mapreduce
 
 import (
 	"fmt"
-	"sync"
 )
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
@@ -107,9 +106,9 @@ func (mr *Master) schedule(phase jobPhase) {
 			idleworker <- newRegister
 		}
 	}()
-	var wg sync.WaitGroup
-
-	wg.Add(ntasks)
+	//var wg sync.WaitGroup
+	doneworks := make(chan bool)
+	//wg.Add(ntasks)
 	for i := 0; i < ntasks; i++ {
 		go func(i int) {
 			args := new(DoTaskArgs)
@@ -125,7 +124,8 @@ func (mr *Master) schedule(phase jobPhase) {
 				debug("worker:%s\n", worker)
 				ok := call(worker, "Worker.DoTask", args, new(struct{}))
 				if ok {
-					wg.Done()
+					//wg.Done()
+					doneworks <- true
 					///放在if里面因为可能是worker本身错误，若是这样放在外面可能还会导致失败(失败继续就是了？？测试看看吧)
 					idleworker <- worker
 					break
@@ -134,7 +134,10 @@ func (mr *Master) schedule(phase jobPhase) {
 
 		}(i)
 	}
-	wg.Wait()
+	//wg.Wait()
+	for i := 0; i < ntasks; i++ {
+		<-doneworks
+	}
 
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
